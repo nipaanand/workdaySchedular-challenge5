dd9ab0ca62e5e5df400fce9196f69b7c44154bc8
nipaanand <nipa.mehta@mail.com>
2022-09-18 19:05:52 -0400

    moment.js script file added

---
+++ script.js
@@ -0,0 +1,5 @@
+// Defining the variable for Save Button
+var saveBtn = $(".saveBtn");
+
+//current day displays after the h1 using moment.js library
+$(".currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
